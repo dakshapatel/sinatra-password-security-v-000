6505e0ff4f427376f90e0f87dec5ae212a6fe20d
Daksha Patel <dakshapatel100@gmail.com>
2018-07-24 00:25:34 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,12 +17,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		user = User.new(:username => params[:username], :password => params[:password])
-		if user.save
-		redirect "/login"
-	else
-		redirect "/failure"
-	end
+		#your code here!
 	end
 
 	get "/login" do
@@ -30,7 +25,7 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/login" do
-		#your code here!
+		user = User.find_by(:username => params[:username])
 	end
 
 	get "/success" do
